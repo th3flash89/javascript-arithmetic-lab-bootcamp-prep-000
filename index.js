f2ebcea2cd2029309efbac41d3160ee308b0ff9c
Trevor LeRiche <hoodooman04@gmail.com>
2019-07-25 23:45:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-function roughScale(x, base) {
-  var parsed = parseInt(x, base);
-  if (isNaN(parsed)) { return 0 }
-  return parsed * 100;
+function add(a,b) {
+  return a + b
 }
-
-console.log(roughScale(' 0xF', 16));
-// expected output: 1500
-
-console.log(roughScale('321', 2));
-// expected output: 0
-
-function circumference(r) {
-  return parseFloat(r) * 2.0 * Math.PI;
+function subtract(a,b) {
+  return a - b
 }
-
-console.log(circumference(4.567));
-// expected output: 28.695307297889173
-
-console.log(circumference('4.567abcdefgh'));
-// expected output: 28.695307297889173
-
-console.log(circumference('abcdefgh'));
-// expected output: NaN
