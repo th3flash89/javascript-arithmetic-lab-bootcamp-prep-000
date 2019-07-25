3c537ed7f3a1db9150ea7821b6a1d7789f4061d9
Trevor LeRiche <hoodooman04@gmail.com>
2019-07-25 23:44:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ console.log(roughScale(' 0xF', 16));
 console.log(roughScale('321', 2));
 // expected output: 0
 
+function circumference(r) {
+  return parseFloat(r) * 2.0 * Math.PI;
+}
+
+console.log(circumference(4.567));
+// expected output: 28.695307297889173
+
+console.log(circumference('4.567abcdefgh'));
+// expected output: 28.695307297889173
+
+console.log(circumference('abcdefgh'));
+// expected output: NaN
